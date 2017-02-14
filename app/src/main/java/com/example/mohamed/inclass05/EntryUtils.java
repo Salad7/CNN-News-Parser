78dd7d53374154f2ed601b7e8d63b7870133830a
salad7 <msalad@uncc.edu>
2017-02-13 19:13:45 -0500

    Modified Async task to work with EntryUtils

--- app/src/main/java/com/example/mohamed/inclass05/EntryUtils.java
+++ app/src/main/java/com/example/mohamed/inclass05/EntryUtils.java
@@ -1,8 +1,44 @@
 package com.example.mohamed.inclass05;
 
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
 /**
  * Created by mohamed on 2/13/17.
  */
 
 public class EntryUtils {
+
+    static public class EntriesSAXParser extends DefaultHandler //Needs to extend Handler
+    {
+        static public ArrayList<Entry> parseEntries(InputStream in){
+
+
+            return null;
+        }
+
+        @Override
+        public void startDocument() throws SAXException {
+            super.startDocument();
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            super.startElement(uri, localName, qName, attributes);
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            super.endElement(uri, localName, qName);
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            super.characters(ch, start, length);
+        }
+    }
 }
--- app/src/main/java/com/example/mohamed/inclass05/GetEntryAsync.java
+++ app/src/main/java/com/example/mohamed/inclass05/GetEntryAsync.java
@@ -2,7 +2,16 @@ package com.example.mohamed.inclass05;
 
 import android.os.AsyncTask;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import static java.lang.System.in;
 
 /**
  * Created by mohamed on 2/13/17.
@@ -29,6 +38,43 @@ public class GetEntryAsync extends AsyncTask<String,Void, ArrayList<Entry>> {
 
     @Override
     protected ArrayList<Entry> doInBackground(String... strings) {
-        return null;
+
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.connect(); //N
+                //reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                //StringBuilder sb = new StringBuilder();
+                //String line = "";
+                //while ((line = reader.readLine()) != null ) {
+                  //  sb.append(line);
+                //}
+                int statusCode = con.getResponseCode();
+                if(statusCode == HttpURLConnection.HTTP_OK){
+                    InputStream in = con.getInputStream();
+                    return EntryUtils.EntriesSAXParser.parseEntries(in);
+                }
+
+                //return DataUtil.DataJSONParser.parseData(sb.toString());
+                return EntryUtils.EntriesSAXParser.parseEntries(in);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            return null;
     }
 }
